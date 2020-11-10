ed138454808f1a0e67708f36c6b303ff425cf871
Felix Leon <felix_hyabusa@hotmail.com>
2020-11-10 11:12:58 -0600

    HATEOAS - First commit

---
+++ springboot-buildingblocks/src/main/java/com/stacksimplify/restservices/springbootbuildingblocks/controllers/OrderHateoasController.java
@@ -0,0 +1,78 @@
+package com.stacksimplify.restservices.springbootbuildingblocks.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.stacksimplify.restservices.springbootbuildingblocks.entities.Order;
+import com.stacksimplify.restservices.springbootbuildingblocks.entities.User;
+import com.stacksimplify.restservices.springbootbuildingblocks.exceptions.OrderNotFoundException;
+import com.stacksimplify.restservices.springbootbuildingblocks.exceptions.UserNotFoundException;
+//import com.stacksimplify.restservices.springbootbuildingblocks.repositories.OrderRepository;
+import com.stacksimplify.restservices.springbootbuildingblocks.repositories.UserRepository;
+import com.stacksimplify.restservices.springbootbuildingblocks.services.OrderService;
+
+@RestController
+@RequestMapping(value = "/users/hateoas")
+public class OrderHateoasController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	//@Autowired
+	//private OrderRepository orderRepository;
+	@Autowired
+	private OrderService orderService;
+	
+	@GetMapping("/{userid}/orders")
+	public CollectionModel<Order> getAllOrders(@PathVariable Long userid) throws UserNotFoundException{
+		
+		Optional <User> userOptional = userRepository.findById(userid);
+		
+		if(!userOptional.isPresent()) {
+			
+			throw new UserNotFoundException("User not found");
+		}
+		
+		List<Order> allOrders = userOptional.get().getOders();
+		
+		CollectionModel<Order> finalCollection = CollectionModel.of(allOrders);
+		
+		return finalCollection;
+		
+	
+	}
+	
+	@GetMapping("/{userid}/orders/{oderid}")
+	@ResponseStatus(HttpStatus.OK)
+	private Optional<Order> getOrderById(@PathVariable Long userid,
+										@PathVariable Long oderid) throws UserNotFoundException{
+		
+		Optional <User> userOptional = userRepository.findById(userid);
+		
+			if(!userOptional.isPresent()) {
+				
+				throw new UserNotFoundException("User not found");
+			}
+		
+			try {
+				return orderService.findOrderById(userid, oderid);
+			}catch(OrderNotFoundException ex) {
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND,ex.getMessage());				
+			}
+		
+	}
+	
+	
+	
+	
+	
+}
---
+++ springboot-buildingblocks/src/main/java/com/stacksimplify/restservices/springbootbuildingblocks/controllers/UserHateoasController.java
@@ -0,0 +1,79 @@
+package com.stacksimplify.restservices.springbootbuildingblocks.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.constraints.Min;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.ControllerLinkBuilder;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.stacksimplify.restservices.springbootbuildingblocks.entities.Order;
+import com.stacksimplify.restservices.springbootbuildingblocks.entities.User;
+import com.stacksimplify.restservices.springbootbuildingblocks.exceptions.UserNotFoundException;
+//import com.stacksimplify.restservices.springbootbuildingblocks.repositories.UserRepository;
+import com.stacksimplify.restservices.springbootbuildingblocks.services.UserService;
+
+@RestController
+@RequestMapping(value = "/hateoas/users")
+@Validated
+public class UserHateoasController {
+
+	// @Autowired
+	// private UserRepository userRepository;
+	@Autowired
+	private UserService userService;
+
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	public CollectionModel<User> getAllUsers() throws UserNotFoundException {
+
+		List<User> allUsers = userService.getAllUsers();
+
+		for (User user : allUsers) {
+			// Self Link
+			Long userid = user.getId();
+			Link selfLink = WebMvcLinkBuilder.linkTo(this.getClass()).slash(userid).withSelfRel();
+			user.add(selfLink);
+			//Build Relationship link between Users and Orders
+			CollectionModel<Order> orders = WebMvcLinkBuilder.methodOn(OrderHateoasController.class)
+					.getAllOrders(userid);
+			Link ordersLink = WebMvcLinkBuilder.linkTo(orders).withRel("all-orders");
+			user.add(ordersLink);
+		}
+		//SelfLink for get all users
+		Link selfLinkGetAllUsers = WebMvcLinkBuilder.linkTo(this.getClass()).withSelfRel();
+		CollectionModel<User> finalCollection = CollectionModel.of(allUsers,selfLinkGetAllUsers);
+		return finalCollection;
+
+	}
+
+	@GetMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public EntityModel<User> getUserById(@PathVariable("id") @Min(1) Long id) {
+		try {
+			Optional<User> userOptional = userService.getUserById(id);
+			User user = userOptional.get();
+			Long userid = user.getId();
+			Link selfLink = WebMvcLinkBuilder.linkTo(this.getClass()).slash(userid).withSelfRel();
+			user.add(selfLink);
+			EntityModel<User> finalResource = EntityModel.of(user);
+			return finalResource;
+		} catch (UserNotFoundException ex) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
+		}
+	}
+
+}
